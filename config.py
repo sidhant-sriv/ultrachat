46236d6d2431f6fda260b79467d4c384d4a81551
sidhant-sriv <sidhant.sriv@gmail.com>
2024-06-12 00:17:46 +0530

    Delete unused config file

--- config.py
+++
@@ -1,7 +0,0 @@
-token = 'MTI0OTc1MzM0NDc1NTYzMDA4MA.GlM0yT.qPyeiNbz3E3qmnz3ehwhUtnNBjZ3hb8xPHIZsI' #Put Your bots token here
-
-prefix = '!' #put prefix here
-
-link = 'https://discord.com/oauth2/authorize?client_id=1249753344755630080&permissions=8&integration_type=0&scope=bot' #put bot invite link here
-
-ownerid = 'sidsodsud' #put your id here
